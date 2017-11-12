82cae15d068a9f1f8f27d143686d250b90063dbf
Larry <github email address>
2017-11-12 13:46:54 -0500

    Created stub of Category class, to hold categories of recipes

--- lib/budget_bytes_cli/category.rb
+++ lib/budget_bytes_cli/category.rb
@@ -1,2 +1,15 @@
 class BudgetBytesCli::Catgeory
+    attr_reader :url, :name
+    
+    @@all = []
+    
+    def self.all
+        @@all
+    end
+    
+    def initialize(url = nil, name = nil)
+        @name = name
+        @url = url
+        @@all << self
+    end
 end
