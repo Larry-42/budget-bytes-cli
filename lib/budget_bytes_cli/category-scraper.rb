516503734bf765c8f49b151460ff206d23729369
Larry <github email address>
2017-11-12 14:49:20 -0500

    Scraped title, url for each recipe category

--- lib/budget_bytes_cli/category-scraper.rb
+++ lib/budget_bytes_cli/category-scraper.rb
@@ -9,5 +9,9 @@ class BudgetBytesCli::CatgeoryScraper
     end
     
     def create_categories
+        locate_categories.each do |item|
+            url = item.css("a").attribute("href").value
+            title = item.css("a").children[0].text
+        end
     end
 end
