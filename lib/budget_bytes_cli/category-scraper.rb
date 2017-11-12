aa8d2ac4080d853580d7a365469909ccf8d641c5
Larry <github email address>
2017-11-12 14:33:56 -0500

    Opens up category page, imports recipe categories

--- lib/budget_bytes_cli/category-scraper.rb
+++ lib/budget_bytes_cli/category-scraper.rb
@@ -1,2 +1,13 @@
 class BudgetBytesCli::CatgeoryScraper
+    
+    def open_page
+        Nokogiri::HTML(open("https://www.budgetbytes.com/recipes/"))
+    end
+    
+    def locate_categories
+        open_page.css(".cat-item")
+    end
+    
+    def create_categories
+    end
 end
