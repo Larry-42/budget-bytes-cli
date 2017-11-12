e1d5ec2380e1db9d1df56c446bec94f16608ec98
Larry <github email address>
2017-11-12 15:32:17 -0500

    Displays categories

--- lib/budget_bytes_cli/cli.rb
+++ lib/budget_bytes_cli/cli.rb
@@ -1,5 +1,17 @@
 class BudgetBytesCli::CLI
     def call
         puts "Welcome to Budget Bytes CLI!"
+        scraper = BudgetBytesCli::CatgeoryScraper.new
+        scraper.create_categories
+        display_items
+    end
+    
+    def display_items
+        puts "Please select a recipe category from the list below:"
+        menu_items = BudgetBytesCli::Catgeory.all.map {|i| i.name}
+        menu_items.each_with_index do |menu_item, idx|
+            puts "#{idx + 1}.  #{menu_item}"
+        end
+        puts "Or enter 'Q' to exit"
     end
 end
