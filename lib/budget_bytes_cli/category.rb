fde5798a3ada48522ca6a83b611c95a43ab6f236
Larry <github email address>
2017-11-12 16:57:02 -0500

    Added code to find number of recipe pages for each category

--- lib/budget_bytes_cli/category.rb
+++ lib/budget_bytes_cli/category.rb
@@ -1,4 +1,5 @@
 class BudgetBytesCli::Catgeory
+    #TODO:  Remove num_pages (only there as placehodler!)
     attr_reader :url, :name, :recipes
     
     @@all = []
@@ -17,12 +18,13 @@ class BudgetBytesCli::Catgeory
         @recipes = []
         recipe_page = Nokogiri::HTML(open(self.url))
         #To enable scraping of pages
-        #page_nums = recipe_page.css(".page-numbers")
-        
-        #TODO:  ADD ROBUST CODE TO FIND LAST PAGE NUMBER
-        
-        #only works if >=3 pages, I think...
-        #last_page = page_nums[page_nums.length-2].text.to_i
+        page_nums = recipe_page.css(".page-numbers")
+        pages_total = 0
+        if page_nums.empty?
+            pages_total = 1
+        else
+            pages_total = page_nums.map{|p| p.text.to_i}.max
+        end
         
         recipe_links = recipe_page.css(".archive-post a")
         recipe_links.each do |r|
