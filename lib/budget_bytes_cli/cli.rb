11e73f7ff3a4580f1a49a0770e49b475c3977ee2
Larry <github email address>
2017-11-12 17:36:51 -0500

    Fixed spelling error in cli.rb, enabled selection of a category

--- lib/budget_bytes_cli/cli.rb
+++ lib/budget_bytes_cli/cli.rb
@@ -1,32 +1,36 @@
 class BudgetBytesCli::CLI
     def call
         puts "Welcome to Budget Bytes CLI!"
-        scraper = BudgetBytesCli::CatgeoryScraper.new
+        scraper = BudgetBytesCli::CategoryScraper.new
         scraper.create_categories
         selection = ""
         while selection != 'Q'
-            selection = get_input
+            selection = get_input_categories
+            if selection != 'Q'
+                sel_num = selection.to_i - 1
+                puts "You picked #{BudgetBytesCli::Category.all[sel_num].name}"
+            end
         end
     end
     
     def display_items
         puts "Please select a recipe category from the list below:"
-        menu_items = BudgetBytesCli::Catgeory.all.map {|i| i.name}
+        menu_items = BudgetBytesCli::Category.all.map {|i| i.name}
         menu_items.each_with_index do |menu_item, idx|
             puts "#{idx + 1}.  #{menu_item}"
         end
         puts "Or enter 'Q' to exit"
     end
     
-    def get_input
+    def get_input_categories
         input = ""
         valid_input = false
-        menu_length = BudgetBytesCli::Catgeory.all.length
+        menu_length = BudgetBytesCli::Category.all.length
         while !valid_input
             display_items
             input = gets.strip.upcase
             if input == 'Q'
-                valid_input = true
+                valid_input = true        #get_recipes_from(self.url)ue
             elsif input.to_i.to_s == input && input.to_i >= 0 && input.to_i <= menu_length
                 valid_input = true
             else
