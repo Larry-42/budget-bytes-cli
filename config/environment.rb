6e85108f8044ee7402955eac607f9ab42e08ea58
Larry <github email address>
2017-11-12 13:40:33 -0500

    Added Category class

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require 'open-uri'
 
 require_relative '../lib/budget_bytes_cli/cli'
 require_relative '../lib/budget_bytes_cli/category-scraper'
+require_relative '../lib/budget_bytes_cli/category'
---
+++ lib/budget_bytes_cli/category.rb
@@ -0,0 +1,2 @@
+class BudgetBytesCli::Catgeory
+end
