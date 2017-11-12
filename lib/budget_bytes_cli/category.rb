023f9db75d303fe2857c8071f3ec1a127be6c9fe
Larry <github email address>
2017-11-12 16:06:05 -0500

    Enabled Category scraping of first page of recipes

--- lib/budget_bytes_cli/category.rb
+++ lib/budget_bytes_cli/category.rb
@@ -1,7 +1,6 @@
 class BudgetBytesCli::Catgeory
-    attr_reader :url, :name
+    attr_reader :url, :name, :recipes
     
-    @recipes = []
     @@all = []
     
     def self.all
@@ -12,5 +11,16 @@ class BudgetBytesCli::Catgeory
         @name = name
         @url = url
         @@all << self
+    end
+    
+    def get_recipes
+        @recipes = []
+        recipe_page = Nokogiri::HTML(open(self.url))
+        recipe_links = recipe_page.css(".archive-post a")
+        recipe_links.each do |r|
+            recipe_title = r.attribute("title").value
+            recipe_url = r.attribute("href").value
+            @recipes << BudgetBytesCli::Recipe.new(recipe_url, recipe_title)
+        end
     end
 end
