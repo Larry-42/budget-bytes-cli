3ec43442e89fb83f47764b010a785dd6842f32a2
Larry <github email address>
2017-11-14 19:37:40 -0500

    Moved category prompting to ArrayPrompter class

--- lib/budget_bytes_cli/array-prompter.rb
+++ lib/budget_bytes_cli/array-prompter.rb
@@ -1,2 +1,36 @@
 class BudgetBytesCli::ArrayPrompter
+    attr_accessor :prompt_text, :array_to_select
+    
+    def last_item
+        self.array_to_select.length
+    end
+    
+    def prompt(text_prompt = nil)
+        if text_prompt
+            self.prompt_text = text_prompt
+        end
+        
+        puts text_prompt
+        self.array_to_select.each_with_index do |menu_item, idx|
+            puts "#{idx + 1}.  #{menu_item}"
+        end
+        puts "Or enter 'Q' to quit"
+    end
+    
+    def get_input
+        input = ""
+        valid_input = false
+        while !valid_input
+            self.prompt
+            input = gets.strip.upcase
+            if input == 'Q'
+                valid_input = true        
+            elsif input.to_i.to_s == input && input.to_i >= 0 && input.to_i <= self.last_item
+                valid_input = true
+            else
+                puts "Invalid input, please try again."
+            end
+        end
+        input
+    end
 end
--- lib/budget_bytes_cli/cli.rb
+++ lib/budget_bytes_cli/cli.rb
@@ -3,9 +3,12 @@ class BudgetBytesCli::CLI
         puts "Welcome to Budget Bytes CLI!"
         scraper = BudgetBytesCli::CategoryScraper.new
         scraper.create_categories
+        category_selector = BudgetBytesCli::ArrayPrompter.new
+        category_selector.array_to_select = BudgetBytesCli::Category.all.map {|i| i.name}
+        category_selector.prompt_text = "Please select a recipe category from the list below:"
         selection = ""
         while selection != 'Q'
-            selection = get_input_categories
+            selection = category_selector.get_input
             if selection != 'Q'
                 selected_category = BudgetBytesCli::Category.all[selection.to_i - 1]
                 display_num_recipes(selected_category)
@@ -22,32 +25,5 @@ class BudgetBytesCli::CLI
         
         puts "You picked #{category_chosen.name}."        
         puts "There are #{num_recipes} recipes in this category."
-    end
-    
-    def display_items
-        puts "Please select a recipe category from the list below:"
-        menu_items = BudgetBytesCli::Category.all.map {|i| i.name}
-        menu_items.each_with_index do |menu_item, idx|
-            puts "#{idx + 1}.  #{menu_item}"
-        end
-        puts "Or enter 'Q' to exit"
-    end
-    
-    def get_input_categories
-        input = ""
-        valid_input = false
-        menu_length = BudgetBytesCli::Category.all.length
-        while !valid_input
-            display_items
-            input = gets.strip.upcase
-            if input == 'Q'
-                valid_input = true        
-            elsif input.to_i.to_s == input && input.to_i >= 0 && input.to_i <= menu_length
-                valid_input = true
-            else
-                puts "Invalid input, please try again."
-            end
-        end
-        input
     end
 end
