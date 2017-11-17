9905e91cb951aec468093b3d2fa5985d9c8417d0
Larry <github email address>
2017-11-16 19:03:40 -0500

    Added recipe selector, if statement in loop to allow for use of recipe selector later

--- lib/budget_bytes_cli/cli.rb
+++ lib/budget_bytes_cli/cli.rb
@@ -7,11 +7,18 @@ class BudgetBytesCli::CLI
         category_selector = BudgetBytesCli::ArrayPrompter.new
         category_selector.array_to_select = BudgetBytesCli::Category.all.map {|i| i.name}
         category_selector.prompt_text = "Selecting recipe category."
+        
+        recipe_selector = BudgetBytesCli::ArrayPrompter.new
+        
+        current_selector = category_selector
         selection = category_selector.get_input
+        
         while selection != 'Q'
-            selected_category = BudgetBytesCli::Category.all[selection.to_i - 1]
-            display_num_recipes(selected_category)
-            selection = category_selector.get_input
+            if current_selector == category_selector
+                selected_category = BudgetBytesCli::Category.all[selection.to_i - 1]
+                display_num_recipes(selected_category)
+                selection = category_selector.get_input
+            end
         end
     end
     
