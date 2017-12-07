6b2b92001a791441dce2c3e0e6d5198bb6722c7d
Larry <github email address>
2017-12-06 19:57:52 -0500

    Moved recipe ingredient, instructions scraping from Recipe class to CategoryScraper

--- lib/budget_bytes_cli/category-scraper.rb
+++ lib/budget_bytes_cli/category-scraper.rb
@@ -49,4 +49,52 @@ class BudgetBytesCli::CategoryScraper
             url + "page/" + num.to_s + "/"
         end
     end
+    
+    #scrape instructions, ingredients from recipe page
+    def self.scrape_recipe(url)
+        page = Nokogiri::HTML(open(url))
+        
+        ingredient_amounts = page.css('.wprm-recipe-ingredient-amount').map {|i| i.text}
+        ingredient_units = page.css('.wprm-recipe-ingredient-unit').map {|i| i.text}
+        ingredient_names = page.css('.wprm-recipe-ingredient-name').map {|i| i.text}
+        
+        ingredient_array = []
+        
+        #scraping ingredients for new site
+        ingredient_amounts.each_with_index do |ele, idx|
+            ingredient_array << [ele, ingredient_units[idx], ingredient_names[idx]].join(' ').strip
+        end
+        
+        #scraping for old site before css switched so that the code above scrapes ingredients
+        old_ingredients_table = page.css("tr")
+        
+        #get rid of first, last rows in table (header and total cost)
+        old_ingredients_table.shift
+        old_ingredients_table.pop 
+        
+        old_ingredients_table.each do |old_ingredient|
+            old_ingredient_text = old_ingredient.text.split("\n")
+            
+            #get rid of first blank item, last cost item
+            old_ingredient_text.shift
+            old_ingredient_text.pop
+            
+            ingredient_array << old_ingredient_text.join(" ")
+        end
+        
+        recipe_steps = []
+        
+        #scraping instructions for old site before css switched to have recipe instructions in own class
+        page.css("p").map {|i| i.text}.each do |p|
+            if p.split(" ")[0] == "STEP"
+                recipe_steps << p.split(" ").slice(2, p.length - 2).join(" ")
+            end
+        end
+        
+        #scraping instructions for new site
+        page.css(".wprm-recipe-instruction-text").each {|i| recipe_steps << i.text}
+
+        [ingredient_array.join("\n"), recipe_steps.join("\n")]
+    end
+    
 end
